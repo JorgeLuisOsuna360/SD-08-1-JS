c9abcf7e2d3f4f4f9518a6c905a3447afa89878a
JorgeOsuna360 <jorgitito09@hotmail.com>
2026-01-06 18:31:50 -0700

    Archivo faltante de la practica de ejercicios - Jorge Osuna

--- task4.js
+++ task4.js
@@ -1,7 +1,24 @@
 export class Player {
-    constructor() {
-      
-    }
-  
-    
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
+
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
+  }
+
+  levelUp() {
+    this.level++;
+  }
+
+}
+
+const jugador1 = new Player("Mario", 5);
+jugador1.info();
+
+jugador1.levelUp();
+jugador1.info();
+
+const jugador2 = new Player("Tara", 6);
+jugador2.info();
